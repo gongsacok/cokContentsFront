fcecc6dd99a21b41cc1a3a8aac5de2bae73016d7
jeonga96 <wazzang96jeonga@gmail.com>
2023-03-09 15:05:01 +0900

    [Feat] appscreenBox slide 추가

--- src/screens/Promotion.js
+++ src/screens/Promotion.js
@@ -1,11 +1,13 @@
 import VisualBox from "../common/component/PromotionVisualBox";
 import UserMentionBox from "../common/component/PromotionUserMentionBox";
+import AppScreenSliderBox from "../common/component/PromotionAppScreenSliderBox";
 
 export default function Promotion() {
   return (
     <div>
       <VisualBox />
       <UserMentionBox />
+      <AppScreenSliderBox />
     </div>
   );
 }
